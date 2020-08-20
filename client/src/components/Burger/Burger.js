8b9e9bd4621fa4278e4f4c56c47fae0895b9472e
njabuloszulu <njabuloszulu@gmail.com>
2020-08-20 11:33:30 +0200

    adding dynamic functionality to the ingredients

--- client/src/components/Burger/Burger.js
+++ client/src/components/Burger/Burger.js
@@ -1,14 +1,19 @@
 import React from "react";
 import BurgerIngredients from "./BurgerIngredients/BurgerIngredients";
-import Classes from './Burger.css'
+import Classes from "./Burger.css";
+// import BurgerBuilder from "../../containers/BurgerBuilder/BurgerBuilder";
 
 const Burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredients key={igKey + i} type={igKey} />;
+    });
+  });
   return (
     <div className={Classes.Burger}>
       <BurgerIngredients type="bread-top" />
-      <BurgerIngredients type="cheese" />
-      <BurgerIngredients type="meat" />
-      <BurgerIngredients type="bread-bottom"/>
+      {transformedIngredients}
+      <BurgerIngredients type="bread-bottom" />
     </div>
   );
 };
