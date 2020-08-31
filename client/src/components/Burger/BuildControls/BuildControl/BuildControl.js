d1b87053cb45da06944e99aed0ef67539f1179f4
njabuloszulu <njabuloszulu@gmail.com>
2020-08-31 15:06:43 +0200

    creating a remove ingredient method for the less button

--- client/src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ client/src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,7 +5,9 @@ const BuildControl = (props) => {
   return (
     <div className={Classes.BuildControl}>
       <div className={Classes.Label}>{props.label}</div>
-      <button className={Classes.Less}>Less</button>
+      <button className={Classes.Less} onClick={props.removed}>
+        Less
+      </button>
       <button className={Classes.More} onClick={props.added}>
         More
       </button>
